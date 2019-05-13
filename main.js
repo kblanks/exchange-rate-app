d9f1f56fe34495c09ca0c5737b9da6915d2b9a92
Kyle (Linux) <kyle.j.blankenbeckler@gmail.com>
2019-05-13 18:48:24 -0500

    starting a cache object to limit API queries

--- main.js
+++ main.js
@@ -1,13 +1,50 @@
+//node js modules
 const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const fs = require('fs');
 
+//setup express framework
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
 
+//initialize variables
 var outputRate=0;
+const cacheFileName='cachedData.json'
+
+var cachedData = {
+}
+
+fs.readFile(cacheFileName, 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log('cache file doesnt exist yet');
+        //writeCacheData(cachedData)
+    }
+    else {
+        cachedData = JSON.parse(data);
+        let currentDate=Date.now();
+        if (cachedData.currentDate!="") {
+            console.log('value exists in cache');
+        }
+        else {
+            console.log('value does not exist in cache');
+        }
+    }
+})
+
+function writeCacheData(data){
+    fs.writeFile(cacheFileName, JSON.stringify(data), 'utf8', function(err){
+        if (err){
+            console.log('Error writing cache file');
+        }
+        else{
+            console.log('Successfully wrote cache file');
+        }
+    });
+}
+
 
 app.get('/', function (req, res){
     res.render('index', {output: null, error: null});
@@ -19,10 +56,9 @@ app.post('/', function (req, res) {
 
     if (outputRate != 0){
         outputText = outputRate*amount;
-                    res.render('index', { output: outputText, error: null});
+        res.render('index', { output: outputText, error: null});
     }
     else {
-        console.log('here');
         request(url, function(err, response, body) {
             if (err) {
                 res.render('index', { output: null, error: 'ERROR - ruh roh'});
@@ -34,6 +70,9 @@ app.post('/', function (req, res) {
                     res.render('index', {output: null, error: 'Error, problem with request'});
                 }
                 else {
+                    cachedData[Date.now()]=outputRate;
+                    writeCacheData(cachedData);
+                    console.log(cachedData);
                     outputText = outputRate*amount;
                     res.render('index', { output: outputText, error: null});
                 }
